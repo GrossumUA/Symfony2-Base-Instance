3c92bfb51c98d40e1583e3f2da7de3397661b14d
Ilya Antipenko <aivus@users.noreply.github.com>
2015-07-01 12:14:42 +0300

    Update xdebug.sh

--- shell/xdebug.sh
+++ shell/xdebug.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-rm /etc/php5/mods-available/xdebug.ini
 echo "zend_extension=/usr/lib/php5/20121212/xdebug.so
     xdebug.default_enable = 1
     xdebug.idekey = "$2"
@@ -10,8 +9,7 @@ echo "zend_extension=/usr/lib/php5/20121212/xdebug.so
     xdebug.remote_log="/var/log/xdebug/xdebug.log"
     xdebug.remote_connect_back=1
     xdebug.max_nesting_level=250
-    xdebug.remote_host = $1" >> /etc/php5/mods-available/xdebug.ini
-
+    xdebug.remote_host = $1" > /etc/php5/mods-available/xdebug.ini
 
 echo "export PHP_IDE_CONFIG="serverName=$3"" >> /home/vagrant/.bashrc
 

2afddb24f7197e59fede1e0976d12c8306c627be
Dmytro Boiko <dboyko@grossum.com>
2015-08-03 11:10:25 +0300

    Added function getParent to extends bundle in folder Application

--- htdocs/src/Application/Sonata/AdminBundle/ApplicationSonataAdminBundle.php
+++ htdocs/src/Application/Sonata/AdminBundle/ApplicationSonataAdminBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ApplicationSonataAdminBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'SonataAdminBundle';
+    }
 }
--- htdocs/src/Application/UserBundle/ApplicationUserBundle.php
+++ htdocs/src/Application/UserBundle/ApplicationUserBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ApplicationUserBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
